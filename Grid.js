efd548406b74bc2ba09257395a2ea19d5026118d
Bartłomiej Bartoszewicz <bartlomiej.bartoszewicz95@gmail.com>
2022-03-18 21:19:43 +0100

    create Tile class

--- Grid.js
+++ Grid.js
@@ -10,9 +10,18 @@ export default class Grid {
     gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
     gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
 
-    this.#cells = createCellElements(gridElement).map(
-      (cellElement, index) => new Cell(cellElement, index % GRID_SIZE, Math.floor(index / GRID_SIZE))
-    );
+    this.#cells = createCellElements(gridElement).map((cellElement, index) => {
+      return new Cell(cellElement, index % GRID_SIZE, Math.floor(index / GRID_SIZE));
+    });
+  }
+
+  get #emptyCells() {
+    return this.#cells.filter((cell) => cell.tile == null);
+  }
+
+  randomEmptyCell() {
+    const randomIndex = Math.floor(Math.random() * this.#emptyCells.length);
+    return this.#emptyCells[randomIndex];
   }
 }
 
@@ -20,11 +29,23 @@ class Cell {
   #cellElement;
   #x;
   #y;
+  #tile;
 
   constructor(cellElement, x, y) {
     this.#cellElement = cellElement;
     this.#x = x;
     this.#y = y;
+  }
+
+  get tile() {
+    return this.#tile;
+  }
+
+  set tile(value) {
+    this.#tile = value;
+    if (value == null) return;
+    this.#tile.x = this.#x;
+    this.#tile.y = this.#y;
   }
 }
 
